52e795fe2f8f6464717fef1e9cb5756d762a7ec5
Shipra8891 <shipramishraa91@gmail.com>
2023-07-29 17:08:18 +0530

    Python learning started

---
+++ pythonLearning/pythonBasic.py
@@ -0,0 +1,64 @@
+print("hello world")
+
+x = 5
+print(x)
+print(x+1)
+print('hello data world !!!!!!')
+print('----------------------------------------')
+print("shipra mishra")
+s= "hello world"
+print('0th index = ',s[0])
+print('1th index = ',s[1])
+print('2th index =',s[2])
+print('3th index =',s[3])
+print('4th index =',s[4])
+print('5th index =',s[5])
+print('6th index =',s[6])
+
+print('----------------------------------------')
+print('-1 th index=',s[-1])
+print('-2 th index=',s[-2])
+print('-5th index =' ,s[-5])
+print('-6th index =',s[-6])
+print('------------------------------------')
+s1 = "hello"
+s2 = "world"
+print(s1 + "  " + s2)
+s1 = "Shipra"
+s2 = "Manish"
+print(s1 + " " + s2)
+print('--------------------------------------')
+print("hello_" * 5)
+print("shipra_" * 5)
+print('---------------------------')
+s = "Namaste World"
+print(s[0:7])
+print(s[8:13])
+print(s[0:13])
+print(s[-5:])
+print(s[-13:])
+print('----------------------------')
+s = "coding world"
+print(s.upper())
+print(s.lower())
+print('-----------------------')
+name = "Shipra"
+age = 30
+married = True
+print("my name is %s, my age is %s,and it is %s that "
+      "I am married" %(name,age, married))
+print(f"my name is {name},and my age is {age}")
+print(f"my name is {name}, and it is {married} that I am married")
+my_string = "Hello, World!"
+print(my_string[7])
+
+print(my_string.replace("H","J"))
+print(my_string.replace("l","k"))
+
+print("---------working with numbers--------")
+x = 4
+y = 2
+print("Result of x/y =" ,x/y)
+print("result of x*y =",x * y)
+print("result of x+y =",x+y)
+print("result of x-y = ",x-y)
