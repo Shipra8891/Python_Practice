f8f20e733ab146e374398aec074c508c248eb275
Shipra8891 <shipramishraa91@gmail.com>
2023-08-06 18:19:59 +0530

    numPy

---
+++ pythonLearning/numPy.py
@@ -0,0 +1,27 @@
+# for installing - pip install numpy
+# ndarray
+# slice it to make python object
+
+import numpy as np # download the required dependency
+print(np.array)
+
+# create an array in python
+list1 = [10,20,30]
+print(np.array(list1))
+print(type(np.array(list1)))
+print(np.array(list1).shape)
+print(np.array(list1).dtype)
+
+list_of_lists = [[5,10,15],[20,25,30],[35,40,45]]
+print(np.array(list_of_lists))
+
+print(np.array(list_of_lists).shape)
+print(np.array(list_of_lists).dtype)
+
+# numPy built in function or methods
+
+
+
+
+
+
