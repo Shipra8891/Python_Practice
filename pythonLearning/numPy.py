c07d9f1e300f900e44d14981ee0d73912e2d311f
Shipra8891 <shipramishraa91@gmail.com>
2023-08-06 18:38:20 +0530

    numPy more examples

--- pythonLearning/numPy.py
+++ pythonLearning/numPy.py
@@ -2,7 +2,8 @@
 # ndarray
 # slice it to make python object
 
-import numpy as np # download the required dependency
+import numpy as np
+# download the required dependency
 print(np.array)
 
 # create an array in python
@@ -19,6 +20,29 @@ print(np.array(list_of_lists).shape)
 print(np.array(list_of_lists).dtype)
 
 # numPy built in function or methods
+
+arrange =np.arange(0,10)
+print(arrange)
+print(np.arange(0,10,2))
+
+# zeroes and ones array
+print(np.zeros((2,3)))
+print(np.ones((2,5)))
+
+#linspace
+print(np.linspace(0,10,7))
+
+# generate random numbers
+
+print(np.random.rand(3,4))
+
+
+print(np.random.randint(4,40,10))
+
+
+
+
+
 
 
 
